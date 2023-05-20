ffbc9afebc066e2586546201ecd4c9dd280f2c01
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-05-20 16:26:32 +0200

    fix(api): add missing base tables when fetching query

--- core/db/queries/query.sql
+++ core/db/queries/query.sql
@@ -10,7 +10,11 @@ SELECT
     base.user_id AS base_user_id,
     base.platform AS base_platform,
     base.kind AS base_kind,
-    base.platform_id AS base_platform_id
+    base.platform_id AS base_platform_id,
+    base.sync_state AS base_sync_state,
+    base.last_sync_start_date AS base_last_sync_start_date,
+    base.last_sync_success_date AS base_last_sync_success_date,
+    base.last_sync_err_msg AS base_last_sync_err_msg
 FROM query
 INNER JOIN base
     ON base.id = query.base_id
