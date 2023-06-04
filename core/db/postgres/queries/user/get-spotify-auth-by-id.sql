588cab2169d5d172bbfd4ddbd27095ecf1442599
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-06-04 16:46:58 +0200

    fix(core): add missing expiration_date column when fetching Spotify auth

--- core/db/postgres/queries/user/get-spotify-auth-by-id.sql
+++ core/db/postgres/queries/user/get-spotify-auth-by-id.sql
@@ -1,6 +1,7 @@
 SELECT
     spotify_auth.user_id AS auth_user_id,
     spotify_auth.email AS auth_email,
+    spotify_auth.expiration_date AS auth_expiration_date,
     spotify_auth.access_token AS auth_access_token,
     spotify_auth.refresh_token AS auth_refresh_token
 FROM spotify_auth
