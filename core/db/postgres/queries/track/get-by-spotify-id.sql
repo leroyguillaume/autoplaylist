8bda33acb626f2be9ad33480c6fe65af8ea19b39
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-06-10 12:16:36 +0200

    fix(core): fetch from_compilation column when fetching track

--- core/db/postgres/queries/track/get-by-spotify-id.sql
+++ core/db/postgres/queries/track/get-by-spotify-id.sql
@@ -2,6 +2,7 @@ SELECT
     track.id AS track_id,
     track.name AS track_name,
     track.release_year AS track_release_year,
+    track.from_compilation AS track_from_compilation,
     track.spotify_id AS track_spotify_id
 FROM track
 WHERE track.spotify_id IS NOT DISTINCT FROM $1;
