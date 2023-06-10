d2800aefae15c03735d66bed8ea1cf6e39f7a48a
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-06-10 11:45:25 +0200

    refactor(core): avoid joining on base table when fetching playlists of user

--- core/db/postgres/queries/playlist/list-by-user.sql
+++ core/db/postgres/queries/playlist/list-by-user.sql
@@ -3,20 +3,8 @@ SELECT
     playlist.creation_date AS playlist_creation_date,
     playlist.user_id AS playlist_user_id,
     playlist.base_id AS playlist_base_id,
-    playlist.name AS playlist_name,
-    base.id AS base_id,
-    base.creation_date AS base_creation_date,
-    base.user_id AS base_user_id,
-    base.platform AS base_platform,
-    base.kind AS base_kind,
-    base.platform_id AS base_platform_id,
-    base.sync_state AS base_sync_state,
-    base.last_sync_start_date AS base_last_sync_start_date,
-    base.last_sync_success_date AS base_last_sync_success_date,
-    base.last_sync_err_msg AS base_last_sync_err_msg
+    playlist.name AS playlist_name
 FROM playlist
-INNER JOIN base
-    ON base.id = playlist.base_id
 WHERE playlist.user_id = $1
 LIMIT $2
 OFFSET $3;
