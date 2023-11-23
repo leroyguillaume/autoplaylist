6c75d00677d1b45ef283a483deb34cdcc665cee1
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-11-23 16:05:03 +0100

    feat(api): implement api

---
+++ api/src/playlist.rs
@@ -0,0 +1,457 @@
+use std::{marker::PhantomData, sync::Arc};
+
+use async_trait::async_trait;
+use autoplaylist_common::{
+    api::{CreatePlaylistRequest, Platform},
+    broker::{
+        rabbitmq::{RabbitMqClient, RabbitMqConsumer},
+        BrokerClient, Consumer, PlaylistMessage, PlaylistMessageKind, SourceMessage,
+        SourceMessageKind,
+    },
+    db::{
+        pg::{PostgresConnection, PostgresPool, PostgresTransaction},
+        DatabaseConnection, DatabasePool, DatabaseTransaction, PlaylistCreation, SourceCreation,
+    },
+    model::{Playlist, Target, User},
+    spotify::{rspotify::RSpotifyClient, SpotifyClient},
+    transactional,
+};
+use tracing::info;
+use uuid::Uuid;
+
+use crate::{ServiceError, ServiceResult};
+
+// PlaylistService
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait PlaylistService: Send + Sync {
+    async fn create(&self, req: CreatePlaylistRequest, owner: User) -> ServiceResult<Playlist>;
+
+    async fn start_synchronization(&self, id: Uuid) -> ServiceResult<()>;
+}
+
+// DefaultPlaylistService
+
+pub struct DefaultPlaylistService<
+    CSM: Consumer,
+    BROKER: BrokerClient<CSM>,
+    DBCONN: DatabaseConnection,
+    DBTX: DatabaseTransaction,
+    DB: DatabasePool<DBCONN, DBTX>,
+    SPOTIFY: SpotifyClient,
+> {
+    broker: Arc<BROKER>,
+    db: Arc<DB>,
+    spotify: Arc<SPOTIFY>,
+    _csm: PhantomData<CSM>,
+    _dbconn: PhantomData<DBCONN>,
+    _dbtx: PhantomData<DBTX>,
+}
+
+impl
+    DefaultPlaylistService<
+        RabbitMqConsumer,
+        RabbitMqClient,
+        PostgresConnection,
+        PostgresTransaction<'_>,
+        PostgresPool,
+        RSpotifyClient,
+    >
+{
+    pub fn new(
+        broker: Arc<RabbitMqClient>,
+        db: Arc<PostgresPool>,
+        spotify: Arc<RSpotifyClient>,
+    ) -> Self {
+        Self {
+            broker,
+            db,
+            spotify,
+            _csm: PhantomData,
+            _dbconn: PhantomData,
+            _dbtx: PhantomData,
+        }
+    }
+}
+
+#[async_trait]
+impl<
+        CSM: Consumer,
+        BROKER: BrokerClient<CSM>,
+        DBCONN: DatabaseConnection,
+        DBTX: DatabaseTransaction,
+        DB: DatabasePool<DBCONN, DBTX>,
+        SPOTIFY: SpotifyClient,
+    > PlaylistService for DefaultPlaylistService<CSM, BROKER, DBCONN, DBTX, DB, SPOTIFY>
+{
+    async fn create(&self, req: CreatePlaylistRequest, mut owner: User) -> ServiceResult<Playlist> {
+        let tgt = match req.platform {
+            Platform::Spotify => {
+                let creds = owner
+                    .creds
+                    .spotify
+                    .as_mut()
+                    .ok_or(ServiceError::NoSpotifyCredentials(owner.id))?;
+                let id = self.spotify.create_playlist(&req.name, creds).await?;
+                Target::Spotify(id)
+            }
+        };
+        let mut db_tx = self.db.begin().await?;
+        let (playlist, new_src) = transactional!(db_tx, async {
+            let src = db_tx.source_by_owner_kind(owner.id, &req.src).await?;
+            let (src, new_src) = match src {
+                Some(src) => (src, false),
+                None => {
+                    let creation = SourceCreation {
+                        kind: req.src,
+                        owner,
+                    };
+                    let src = db_tx.create_source(&creation).await?;
+                    info!(src.owner.email, %src.owner.id, "source created");
+                    (src, true)
+                }
+            };
+            let creation = PlaylistCreation {
+                name: req.name,
+                predicate: req.predicate,
+                src,
+                tgt,
+            };
+            let playlist = db_tx.create_playlist(&creation).await?;
+            info!(%playlist.id, playlist.src.owner.email, %playlist.src.owner.id, "playlist created");
+            Ok::<(Playlist, bool), ServiceError>((playlist, new_src))
+        })?;
+        let publisher = self.broker.publisher();
+        if new_src {
+            let msg = SourceMessage {
+                id: playlist.src.id,
+                kind: SourceMessageKind::Created,
+            };
+            publisher.publish_source_message(&msg).await?;
+        }
+        let msg = PlaylistMessage {
+            id: playlist.src.id,
+            kind: PlaylistMessageKind::Created,
+        };
+        publisher.publish_playlist_message(&msg).await?;
+        Ok(playlist)
+    }
+
+    async fn start_synchronization(&self, id: Uuid) -> ServiceResult<()> {
+        let msg = PlaylistMessage {
+            id,
+            kind: PlaylistMessageKind::Sync,
+        };
+        self.broker
+            .publisher()
+            .publish_playlist_message(&msg)
+            .await?;
+        Ok(())
+    }
+}
+
+// Tests
+
+#[cfg(test)]
+mod test {
+    use autoplaylist_common::{
+        broker::{MockBrokerClient, MockPublisher},
+        db::{MockDatabasePool, MockDatabaseTransaction},
+        model::{
+            Credentials, Playlist, Predicate, Role, Source, SourceKind, SpotifyCredentials,
+            SpotifyResourceKind, SpotifyToken, Synchronization,
+        },
+        spotify::MockSpotifyClient,
+    };
+    use chrono::Utc;
+    use mockable::Mock;
+    use mockall::predicate::eq;
+    use uuid::Uuid;
+
+    use super::*;
+
+    mod default_playlist_message {
+        use super::*;
+
+        mod create {
+            use super::*;
+
+            // Data
+
+            struct Data {
+                owner: User,
+                req: CreatePlaylistRequest,
+                spotify_creds: SpotifyCredentials,
+            }
+
+            // Mocks
+
+            #[derive(Clone, Default)]
+            struct Mocks {
+                create_spotify_playlist: Mock<()>,
+                create_src: Mock<()>,
+                publish_playlist_msg: Mock<()>,
+                publish_src_msg: Mock<()>,
+                src_by_owner_kind: Mock<Option<Source>, Source>,
+            }
+
+            // run
+
+            async fn run(data: Data, mocks: Mocks) -> ServiceResult<(Playlist, Playlist)> {
+                let spotify_id = "id";
+                let expected = Playlist {
+                    creation: Utc::now(),
+                    id: Uuid::new_v4(),
+                    name: data.req.name.clone(),
+                    predicate: data.req.predicate.clone(),
+                    src: Source {
+                        creation: Utc::now(),
+                        id: Uuid::new_v4(),
+                        kind: data.req.src.clone(),
+                        owner: data.owner.clone(),
+                        sync: Synchronization::Pending,
+                    },
+                    sync: Synchronization::Pending,
+                    tgt: Target::Spotify(spotify_id.into()),
+                };
+                let src_msg = SourceMessage {
+                    id: expected.src.id,
+                    kind: SourceMessageKind::Created,
+                };
+                let playlist_msg = PlaylistMessage {
+                    id: expected.src.id,
+                    kind: PlaylistMessageKind::Created,
+                };
+                let mut spotify = MockSpotifyClient::new();
+                spotify
+                    .expect_create_playlist()
+                    .with(eq(expected.name.clone()), eq(data.spotify_creds.clone()))
+                    .times(mocks.create_spotify_playlist.times())
+                    .returning(|_, _| Ok(spotify_id.into()));
+                let db = MockDatabasePool {
+                    begin: Mock::once({
+                        let playlist = expected.clone();
+                        let mocks = mocks.clone();
+                        move || {
+                            let mut tx = MockDatabaseTransaction::new();
+                            tx.client
+                                .expect_source_by_owner_kind()
+                                .with(eq(playlist.src.owner.id), eq(playlist.src.kind.clone()))
+                                .times(mocks.src_by_owner_kind.times())
+                                .returning({
+                                    let src = playlist.src.clone();
+                                    let mock = mocks.src_by_owner_kind.clone();
+                                    move |_, _| Ok(mock.call_with_args(src.clone()))
+                                });
+                            let creation: SourceCreation = playlist.src.clone().into();
+                            tx.client
+                                .expect_create_source()
+                                .with(eq(creation))
+                                .times(mocks.create_src.times())
+                                .returning({
+                                    let src = playlist.src.clone();
+                                    move |_| Ok(src.clone())
+                                });
+                            let creation: PlaylistCreation = playlist.clone().into();
+                            tx.client
+                                .expect_create_playlist()
+                                .with(eq(creation))
+                                .returning({
+                                    let playlist = playlist.clone();
+                                    move |_| Ok(playlist.clone())
+                                });
+                            tx
+                        }
+                    }),
+                    ..Default::default()
+                };
+                let mut publisher = MockPublisher::new();
+                publisher
+                    .expect_publish_source_message()
+                    .with(eq(src_msg))
+                    .times(mocks.publish_src_msg.times())
+                    .returning(|_| Ok(()));
+                publisher
+                    .expect_publish_playlist_message()
+                    .with(eq(playlist_msg))
+                    .times(mocks.publish_playlist_msg.times())
+                    .returning(|_| Ok(()));
+                let broker = MockBrokerClient {
+                    publisher,
+                    ..Default::default()
+                };
+                let playlist_svc = DefaultPlaylistService {
+                    broker: Arc::new(broker),
+                    db: Arc::new(db),
+                    spotify: Arc::new(spotify),
+                    _csm: PhantomData,
+                    _dbconn: PhantomData,
+                    _dbtx: PhantomData,
+                };
+                playlist_svc
+                    .create(data.req, data.owner)
+                    .await
+                    .map(|playlist| (playlist, expected))
+            }
+
+            // Tests
+
+            #[tokio::test]
+            async fn no_spotify_credentials() {
+                let owner_id = Uuid::new_v4();
+                let spotify_creds = SpotifyCredentials {
+                    id: "id".into(),
+                    token: SpotifyToken {
+                        access: "access".into(),
+                        expiration: Utc::now(),
+                        refresh: "refresh".into(),
+                    },
+                };
+                let data = Data {
+                    owner: User {
+                        creation: Utc::now(),
+                        creds: Default::default(),
+                        email: "user@test".into(),
+                        id: owner_id,
+                        role: Role::User,
+                    },
+                    req: CreatePlaylistRequest {
+                        name: "name".into(),
+                        platform: Platform::Spotify,
+                        predicate: Predicate::YearEquals(1993),
+                        src: SourceKind::Spotify(SpotifyResourceKind::SavedTracks),
+                    },
+                    spotify_creds,
+                };
+                let mocks = Mocks::default();
+                let err = run(data, mocks)
+                    .await
+                    .expect_err("creating playlist should fail");
+                match err {
+                    ServiceError::NoSpotifyCredentials(id) => {
+                        assert_eq!(id, owner_id);
+                    }
+                    _ => panic!("unexpected error: {err}"),
+                }
+            }
+
+            #[tokio::test]
+            async fn spotify_playlist_when_source_didnt_exist() {
+                let owner_id = Uuid::new_v4();
+                let spotify_creds = SpotifyCredentials {
+                    id: "id".into(),
+                    token: SpotifyToken {
+                        access: "access".into(),
+                        expiration: Utc::now(),
+                        refresh: "refresh".into(),
+                    },
+                };
+                let data = Data {
+                    owner: User {
+                        creation: Utc::now(),
+                        creds: Credentials {
+                            spotify: Some(spotify_creds.clone()),
+                        },
+                        email: "user@test".into(),
+                        id: owner_id,
+                        role: Role::User,
+                    },
+                    req: CreatePlaylistRequest {
+                        name: "name".into(),
+                        platform: Platform::Spotify,
+                        predicate: Predicate::YearEquals(1993),
+                        src: SourceKind::Spotify(SpotifyResourceKind::SavedTracks),
+                    },
+                    spotify_creds,
+                };
+                let mocks = Mocks {
+                    create_spotify_playlist: Mock::once(|| ()),
+                    create_src: Mock::once(|| ()),
+                    publish_playlist_msg: Mock::once(|| ()),
+                    publish_src_msg: Mock::once(|| ()),
+                    src_by_owner_kind: Mock::once_with_args(|_| None),
+                };
+                let (playlist, expected) =
+                    run(data, mocks).await.expect("failed to create playlist");
+                assert_eq!(playlist, expected);
+            }
+
+            #[tokio::test]
+            async fn spotify_playlist_when_source_already_existed() {
+                let owner_id = Uuid::new_v4();
+                let spotify_creds = SpotifyCredentials {
+                    id: "id".into(),
+                    token: SpotifyToken {
+                        access: "access".into(),
+                        expiration: Utc::now(),
+                        refresh: "refresh".into(),
+                    },
+                };
+                let data = Data {
+                    owner: User {
+                        creation: Utc::now(),
+                        creds: Credentials {
+                            spotify: Some(spotify_creds.clone()),
+                        },
+                        email: "user@test".into(),
+                        id: owner_id,
+                        role: Role::User,
+                    },
+                    req: CreatePlaylistRequest {
+                        name: "name".into(),
+                        platform: Platform::Spotify,
+                        predicate: Predicate::YearEquals(1993),
+                        src: SourceKind::Spotify(SpotifyResourceKind::SavedTracks),
+                    },
+                    spotify_creds,
+                };
+                let mocks = Mocks {
+                    create_spotify_playlist: Mock::once(|| ()),
+                    publish_playlist_msg: Mock::once(|| ()),
+                    src_by_owner_kind: Mock::once_with_args(|src: Source| Some(src.clone())),
+                    ..Default::default()
+                };
+                let (playlist, expected) =
+                    run(data, mocks).await.expect("failed to create playlist");
+                assert_eq!(playlist, expected);
+            }
+        }
+
+        mod start_synchronization {
+            use super::*;
+
+            // Tests
+
+            #[tokio::test]
+            async fn unit() {
+                let msg = PlaylistMessage {
+                    id: Uuid::new_v4(),
+                    kind: PlaylistMessageKind::Sync,
+                };
+                let mut publisher = MockPublisher::new();
+                publisher
+                    .expect_publish_playlist_message()
+                    .with(eq(msg.clone()))
+                    .times(1)
+                    .returning(|_| Ok(()));
+                let broker = MockBrokerClient {
+                    publisher,
+                    ..Default::default()
+                };
+                let playlist_svc = DefaultPlaylistService {
+                    broker: Arc::new(broker),
+                    db: Arc::new(MockDatabasePool::default()),
+                    spotify: Arc::new(MockSpotifyClient::default()),
+                    _csm: PhantomData,
+                    _dbconn: PhantomData,
+                    _dbtx: PhantomData,
+                };
+                playlist_svc
+                    .start_synchronization(msg.id)
+                    .await
+                    .expect("failed to start synchronization");
+            }
+        }
+    }
+}
---
+++ common/src/api.rs
@@ -0,0 +1,245 @@
+use chrono::{DateTime, Utc};
+use enum_display::EnumDisplay;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+use crate::model::{
+    Playlist, PlaylistSynchronization, Predicate, Source, SourceKind, SourceSynchronization, Target,
+};
+
+// Consts - Paths
+
+pub const PATH_AUTH_SPOTIFY: &str = "/auth/spotify";
+pub const PATH_AUTH_SPOTIFY_TOKEN: &str = "/auth/spotify/token";
+pub const PATH_HEALTH: &str = "/health";
+pub const PATH_PLAYLIST: &str = "/playlist";
+pub const PATH_SRC: &str = "/source";
+pub const PATH_SYNC: &str = "/sync";
+
+// AuthenticateViaSpotifyQueryParams
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct AuthenticateViaSpotifyQueryParams {
+    pub code: String,
+    pub redirect_uri: String,
+}
+
+// CreatePlaylistRequest
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CreatePlaylistRequest {
+    pub name: String,
+    pub platform: Platform,
+    pub predicate: Predicate,
+    pub src: SourceKind,
+}
+
+// Platform
+
+#[derive(Clone, Copy, Debug, Deserialize, EnumDisplay, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[enum_display(case = "Kebab")]
+pub enum Platform {
+    Spotify,
+}
+
+// PlaylistResponse
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PlaylistResponse {
+    pub creation: DateTime<Utc>,
+    pub id: Uuid,
+    pub name: String,
+    pub predicate: Predicate,
+    pub src: SourceResponse,
+    pub sync: PlaylistSynchronization,
+    pub tgt: Target,
+}
+
+impl From<Playlist> for PlaylistResponse {
+    fn from(playlist: Playlist) -> Self {
+        Self {
+            creation: playlist.creation,
+            id: playlist.id,
+            name: playlist.name,
+            predicate: playlist.predicate,
+            src: playlist.src.into(),
+            sync: playlist.sync,
+            tgt: playlist.tgt,
+        }
+    }
+}
+
+// JwtResponse
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JwtResponse {
+    pub jwt: String,
+}
+
+impl From<String> for JwtResponse {
+    fn from(jwt: String) -> Self {
+        Self { jwt }
+    }
+}
+
+// RedirectUriQueryParam
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct RedirectUriQueryParam {
+    pub redirect_uri: String,
+}
+
+impl From<String> for RedirectUriQueryParam {
+    fn from(redirect_uri: String) -> Self {
+        Self { redirect_uri }
+    }
+}
+
+// SourceResponse
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SourceResponse {
+    pub creation: DateTime<Utc>,
+    pub id: Uuid,
+    pub kind: SourceKind,
+    pub owner: Uuid,
+    pub sync: SourceSynchronization,
+}
+
+impl From<Source> for SourceResponse {
+    fn from(src: Source) -> Self {
+        Self {
+            creation: src.creation,
+            id: src.id,
+            kind: src.kind,
+            owner: src.owner.id,
+            sync: src.sync,
+        }
+    }
+}
+
+// Tests
+
+#[cfg(test)]
+mod test {
+    use crate::model::{Role, SpotifyResourceKind, Target, User};
+
+    use super::*;
+
+    mod playlist_response {
+        use super::*;
+
+        mod from_playlist {
+            use super::*;
+
+            #[test]
+            fn response() {
+                let playlist = Playlist {
+                    creation: Utc::now(),
+                    id: Uuid::new_v4(),
+                    name: "name".into(),
+                    predicate: Predicate::YearEquals(1993),
+                    src: Source {
+                        creation: Utc::now(),
+                        id: Uuid::new_v4(),
+                        kind: SourceKind::Spotify(SpotifyResourceKind::SavedTracks),
+                        owner: User {
+                            creation: Utc::now(),
+                            creds: Default::default(),
+                            email: "user@test".into(),
+                            id: Uuid::new_v4(),
+                            role: Role::User,
+                        },
+                        sync: SourceSynchronization::Pending,
+                    },
+                    sync: PlaylistSynchronization::Pending,
+                    tgt: Target::Spotify("id".into()),
+                };
+                let expected = PlaylistResponse {
+                    creation: playlist.creation,
+                    id: playlist.id,
+                    name: playlist.name.clone(),
+                    predicate: playlist.predicate.clone(),
+                    src: playlist.src.clone().into(),
+                    sync: playlist.sync.clone(),
+                    tgt: playlist.tgt.clone(),
+                };
+                let resp = PlaylistResponse::from(playlist);
+                assert_eq!(resp, expected);
+            }
+        }
+    }
+
+    mod jwt_response {
+        use super::*;
+
+        mod from_string {
+            use super::*;
+
+            #[test]
+            fn response() {
+                let jwt = "jwt";
+                let expected = JwtResponse { jwt: jwt.into() };
+                let resp = JwtResponse::from(String::from(jwt));
+                assert_eq!(resp, expected);
+            }
+        }
+    }
+
+    mod redirect_uri_param {
+        use super::*;
+
+        mod from_string {
+            use super::*;
+
+            #[test]
+            fn param() {
+                let redirect_uri = "uri";
+                let expected = RedirectUriQueryParam {
+                    redirect_uri: redirect_uri.into(),
+                };
+                let param = RedirectUriQueryParam::from(String::from(redirect_uri));
+                assert_eq!(param, expected);
+            }
+        }
+    }
+
+    mod source_response {
+        use super::*;
+
+        mod from_source {
+            use super::*;
+
+            #[test]
+            fn response() {
+                let src = Source {
+                    creation: Utc::now(),
+                    id: Uuid::new_v4(),
+                    kind: SourceKind::Spotify(SpotifyResourceKind::SavedTracks),
+                    owner: User {
+                        creation: Utc::now(),
+                        creds: Default::default(),
+                        email: "user@test".into(),
+                        id: Uuid::new_v4(),
+                        role: Role::User,
+                    },
+                    sync: SourceSynchronization::Pending,
+                };
+                let expected = SourceResponse {
+                    creation: src.creation,
+                    id: src.id,
+                    kind: src.kind.clone(),
+                    owner: src.owner.id,
+                    sync: src.sync.clone(),
+                };
+                let resp = SourceResponse::from(src);
+                assert_eq!(resp, expected);
+            }
+        }
+    }
+}
