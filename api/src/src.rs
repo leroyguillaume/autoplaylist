cea480e8259ba7a274f362eb05f06bc3815bad02
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2023-12-03 15:08:11 +0100

    feat: add command to list sources

--- api/src/src.rs
+++ api/src/src.rs
@@ -1,9 +1,16 @@
 use std::{marker::PhantomData, sync::Arc};
 
 use async_trait::async_trait;
-use autoplaylist_common::broker::{
-    rabbitmq::{RabbitMqClient, RabbitMqConsumer},
-    BrokerClient, Consumer, SourceMessage, SourceMessageKind,
+use autoplaylist_common::{
+    broker::{
+        rabbitmq::{RabbitMqClient, RabbitMqConsumer},
+        BrokerClient, Consumer, SourceMessage, SourceMessageKind,
+    },
+    db::{
+        pg::{PostgresConnection, PostgresPool, PostgresTransaction},
+        DatabaseConnection, DatabasePool, DatabaseTransaction,
+    },
+    model::{Page, PageRequest, Source},
 };
 use uuid::Uuid;
 
@@ -14,27 +21,78 @@ use crate::ServiceResult;
 #[cfg_attr(test, mockall::automock)]
 #[async_trait]
 pub trait SourceService: Send + Sync {
+    async fn authenticated_user_sources(
+        &self,
+        user_id: Uuid,
+        req: PageRequest,
+    ) -> ServiceResult<Page<Source>>;
+
+    async fn sources(&self, req: PageRequest) -> ServiceResult<Page<Source>>;
+
     async fn start_synchronization(&self, id: Uuid) -> ServiceResult<()>;
 }
 
 // DefaultSourceService
 
-pub struct DefaultSourceService<CSM: Consumer, BROKER: BrokerClient<CSM>> {
+pub struct DefaultSourceService<
+    CSM: Consumer,
+    BROKER: BrokerClient<CSM>,
+    DBCONN: DatabaseConnection,
+    DBTX: DatabaseTransaction,
+    DB: DatabasePool<DBCONN, DBTX>,
+> {
     broker: Arc<BROKER>,
+    db: Arc<DB>,
     _csm: PhantomData<CSM>,
+    _dbconn: PhantomData<DBCONN>,
+    _dbtx: PhantomData<DBTX>,
 }
 
-impl DefaultSourceService<RabbitMqConsumer, RabbitMqClient> {
-    pub fn new(broker: Arc<RabbitMqClient>) -> Self {
+impl
+    DefaultSourceService<
+        RabbitMqConsumer,
+        RabbitMqClient,
+        PostgresConnection,
+        PostgresTransaction<'_>,
+        PostgresPool,
+    >
+{
+    pub fn new(broker: Arc<RabbitMqClient>, db: Arc<PostgresPool>) -> Self {
         Self {
             broker,
+            db,
             _csm: PhantomData,
+            _dbconn: PhantomData,
+            _dbtx: PhantomData,
         }
     }
 }
 
 #[async_trait]
-impl<CSM: Consumer, BROKER: BrokerClient<CSM>> SourceService for DefaultSourceService<CSM, BROKER> {
+impl<
+        CSM: Consumer,
+        BROKER: BrokerClient<CSM>,
+        DBCONN: DatabaseConnection,
+        DBTX: DatabaseTransaction,
+        DB: DatabasePool<DBCONN, DBTX>,
+    > SourceService for DefaultSourceService<CSM, BROKER, DBCONN, DBTX, DB>
+{
+    async fn authenticated_user_sources(
+        &self,
+        user_id: Uuid,
+        req: PageRequest,
+    ) -> ServiceResult<Page<Source>> {
+        let mut db_conn = self.db.acquire().await?;
+        let page = db_conn.user_sources(user_id, req).await?;
+        Ok(page)
+    }
+
+    async fn sources(&self, req: PageRequest) -> ServiceResult<Page<Source>> {
+        let mut db_conn = self.db.acquire().await?;
+        let page = db_conn.sources(req).await?;
+        Ok(page)
+    }
+
     async fn start_synchronization(&self, id: Uuid) -> ServiceResult<()> {
         let msg = SourceMessage {
             id,
@@ -49,7 +107,11 @@ impl<CSM: Consumer, BROKER: BrokerClient<CSM>> SourceService for DefaultSourceSe
 
 #[cfg(test)]
 mod test {
-    use autoplaylist_common::broker::{MockBrokerClient, MockPublisher};
+    use autoplaylist_common::{
+        broker::{MockBrokerClient, MockPublisher},
+        db::{MockDatabaseConnection, MockDatabasePool},
+    };
+    use mockable::Mock;
     use mockall::predicate::eq;
     use uuid::Uuid;
 
@@ -57,6 +119,96 @@ mod test {
 
     mod default_source_message {
         use super::*;
+
+        mod authenticated_user_sources {
+            use super::*;
+
+            // Tests
+
+            #[tokio::test]
+            async fn page() {
+                let id = Uuid::new_v4();
+                let req = PageRequest::new(10, 0);
+                let expected = Page {
+                    first: true,
+                    items: vec![],
+                    last: true,
+                    req,
+                    total: 0,
+                };
+                let db = MockDatabasePool {
+                    acquire: Mock::once({
+                        let expected = expected.clone();
+                        move || {
+                            let mut conn = MockDatabaseConnection::new();
+                            conn.0
+                                .expect_user_sources()
+                                .with(eq(id), eq(req))
+                                .times(1)
+                                .returning({
+                                    let expected = expected.clone();
+                                    move |_, _| Ok(expected.clone())
+                                });
+                            conn
+                        }
+                    }),
+                    ..Default::default()
+                };
+                let src_svc = DefaultSourceService {
+                    broker: Arc::new(MockBrokerClient::default()),
+                    db: Arc::new(db),
+                    _csm: PhantomData,
+                    _dbconn: PhantomData,
+                    _dbtx: PhantomData,
+                };
+                let page = src_svc
+                    .authenticated_user_sources(id, req)
+                    .await
+                    .expect("failed to get sources");
+                assert_eq!(page, expected);
+            }
+        }
+
+        mod sources {
+            use super::*;
+
+            // Tests
+
+            #[tokio::test]
+            async fn page() {
+                let req = PageRequest::new(10, 0);
+                let expected = Page {
+                    first: true,
+                    items: vec![],
+                    last: true,
+                    req,
+                    total: 0,
+                };
+                let db = MockDatabasePool {
+                    acquire: Mock::once({
+                        let expected = expected.clone();
+                        move || {
+                            let mut conn = MockDatabaseConnection::new();
+                            conn.0.expect_sources().with(eq(req)).times(1).returning({
+                                let expected = expected.clone();
+                                move |_| Ok(expected.clone())
+                            });
+                            conn
+                        }
+                    }),
+                    ..Default::default()
+                };
+                let src_svc = DefaultSourceService {
+                    broker: Arc::new(MockBrokerClient::default()),
+                    db: Arc::new(db),
+                    _csm: PhantomData,
+                    _dbconn: PhantomData,
+                    _dbtx: PhantomData,
+                };
+                let page = src_svc.sources(req).await.expect("failed to get sources");
+                assert_eq!(page, expected);
+            }
+        }
 
         mod start_synchronization {
             use super::*;
@@ -81,7 +233,10 @@ mod test {
                 };
                 let src_svc = DefaultSourceService {
                     broker: Arc::new(broker),
+                    db: Arc::new(MockDatabasePool::default()),
                     _csm: PhantomData,
+                    _dbconn: PhantomData,
+                    _dbtx: PhantomData,
                 };
                 src_svc
                     .start_synchronization(msg.id)
---
+++ common/resources/main/db/pg/queries/count-user-sources.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(id)
+FROM source
+WHERE owner = $1;
